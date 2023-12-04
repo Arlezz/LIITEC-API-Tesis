0145de45acc5ac87d600e699b27a197e7c5db04e
Arlezz <antonyrodriguezc.2001@gmail.com>
2023-12-04 14:19:18 -0300

    Add swagger documentation

--- server/routes/data.routes.js
+++ server/routes/data.routes.js
@@ -3,7 +3,7 @@ const router = Router();
 const DataController = require('../controller/DataController');
 
 //User levels authorization
-const { USER_LEVEL_0, USER_LEVEL_1, USER_LEVEL_2 } = process.env;
+const { USER_LEVEL_2 } = process.env;
 
 //Middleware
 const authorization = require('../auth/apiAuth');
@@ -19,10 +19,6 @@ router.get("/data/:deviceId/agregate", authorization.requireAPIKeyOfType(USER_LE
 
 //Get last data from a device
 router.get("/data/:deviceId/last", authorization.requireAPIKeyOfType(USER_LEVEL_2),DataController.getLastDataFromDevice);
-
-
-
-
 
 
 
