b26bdc0a90ce30d5ef99268e49ca447c29214016
Arlezz <antonyrodriguezc.2001@gmail.com>
2023-12-27 19:24:59 -0300

    Refactor code for improved performance and readability

---
+++ server/models/channel.model.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const { Schema } = mongoose;
+
+
+const channelSchema = new Schema(
+    {
+        channelId: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        name: { 
+            type: String, 
+            //required: true 
+        },
+        isActive: { 
+            type: Boolean, 
+            default: true 
+        },
+        isPublic: { 
+            type: Boolean, 
+            default: false 
+        },
+        description: { 
+            type: String, 
+            //required: true 
+        },
+        owner: {
+            type: ObjectId,
+            ref: 'user', 
+            required: true
+        },
+        project: {
+            type: String,
+            //required: true
+        },
+        ubication: {
+            latitude: {
+                type: Number,
+            },
+            longitude: {
+                type: Number,
+            }
+        },
+        createdOn: {
+            type: Date,
+            default: Date.now,
+            required: true
+        },
+        updatedOn: {
+            type: Date,
+            default: Date.now,
+            required: true
+        }
+    }
+);
+    
+module.exports = mongoose.model('channel', channelSchema);
